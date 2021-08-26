3e4092658922a16c09d637655b516151a413ee27
restuardananto <stuard.phone@gmail.com>
2021-08-26 22:11:23 +0800

    dashboard peserta 50%

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,15 @@
-@extends('layouts.backend')
+@extends('layouts.backendpeserta')
 
 @section('content')
     <!-- Hero -->
-    <div class="bg-body-light">
+    <div class="bg-body">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Chemist Fun Days</h1>
+                <h1 class="flex-sm-fill font-size-h4 font-w400 mt-0 mb-0 mb-sm-2">Selamat datang (nanti tolong masukin nama user)</h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item">App</li>
-                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                        <li class="breadcrumb-item"></li>Chemist Fun Days
+                        <li class="breadcrumb-item active" aria-current="page">Beranda</li>
                     </ol>
                 </nav>
             </div>
@@ -20,22 +20,69 @@
     <!-- Page Content -->
     <div class="content">
         <div class="row justify-content-center">
-            <div class="col-md-6 col-xl-5">
+            <div class="col-md-6 col-xl-3">
                 <div class="block block-rounded">
                     <div class="block-header block-header-default">
-                        <h3 class="block-title">Welcome to your app</h3>
+                        <h3 class="block-title row justify-content-center">Status Berkas</h3>
                     </div>
                     <div class="block-content">
                         <p>
-                            We’ve put everything together, so you can start working on your Laravel project as soon as possible! Dashmix assets are integrated and work seamlessly with Laravel Mix, so you can use the npm scripts as you would in any other Laravel project.
+                            diisi dengan data berkas yang telah di upload oleh peserta
                         </p>
                         <p>
-                            Feel free to use any examples you like from the full versions to build your own pages. <strong>Wish you all the best and happy coding!</strong>
+                            <strong>Status berkas</strong>
                         </p>
                     </div>
                 </div>
             </div>
+            <div class="col-md-6 col-xl-3">
+                <div class="block block-rounded">
+                    <div class="block-header block-header-default">
+                        <h3 class="block-title row justify-content-center">Status Berkas</h3>
+                    </div>
+                    <div class="block-content">
+                        <p>
+                            diisi dengan data berkas yang telah di upload oleh peserta
+                        </p>
+                        <p>
+                            <strong>Status berkas</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-3">
+                <div class="block block-rounded">
+                    <div class="block-header block-header-default">
+                        <h3 class="block-title row justify-content-center">Status Berkas</h3>
+                    </div>
+                    <div class="block-content">
+                        <p>
+                            diisi dengan data berkas yang telah di upload oleh peserta
+                        </p>
+                        <p>
+                            <strong>Status berkas</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-3">
+                <div class="block block-rounded">
+                    <div class="block-header block-header-default">
+                        <h3 class="block-title row justify-content-center">Status Berkas</h3>
+                    </div>
+                    <div class="block-content">
+                        <p>
+                            diisi dengan data berkas yang telah di upload oleh peserta
+                        </p>
+                        <p>
+                            <strong>Status berkas</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
+        
     </div>
     <!-- END Page Content -->
 @endsection
---
+++ resources/views/layouts/backendpeserta.blade.php
@@ -0,0 +1,328 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+        <title>Dashmix - Bootstrap 4 Admin Template &amp; UI Framework</title>
+
+        <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Icons -->
+        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+        <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
+        <!-- Fonts and Styles -->
+        @yield('css_before')
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
+        <link rel="stylesheet" id="css-main" href="{{ mix('css/dashmix.css') }}">
+
+        <!-- You can include a specific file from public/css/themes/ folder to alter the default color theme of the template. eg: -->
+        <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('css/themes/xwork.css') }}"> -->
+        @yield('css_after')
+
+        <!-- Scripts -->
+        <script>window.Laravel = {!! json_encode(['csrfToken' => csrf_token(),]) !!};</script>
+    </head>
+    <body>
+        <!-- Page Container -->
+        <!--
+            Available classes for #page-container:
+
+        GENERIC
+
+            'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
+
+        SIDEBAR & SIDE OVERLAY
+
+            'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+            'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+            'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+            'sidebar-dark'                              Dark themed sidebar
+
+            'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+            'side-overlay-o'                            Visible Side Overlay by default
+
+            'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+            'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+        HEADER
+
+            ''                                          Static Header if no class is added
+            'page-header-fixed'                         Fixed Header
+
+
+        Footer
+
+            ''                                          Static Footer if no class is added
+            'page-footer-fixed'                         Fixed Footer (please have in mind that the footer has a specific height when is fixed)
+
+        HEADER STYLE
+
+            ''                                          Classic Header style if no class is added
+            'page-header-dark'                          Dark themed Header
+            'page-header-glass'                         Light themed Header with transparency by default
+                                                        (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+            'page-header-glass page-header-dark'         Dark themed Header with transparency by default
+                                                        (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+        MAIN CONTENT LAYOUT
+
+            ''                                          Full width Main Content if no class is added
+            'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+            'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+        -->
+        <div id="page-container" class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-narrow">
+
+            <!-- Sidebar -->
+            <!--
+                Sidebar Mini Mode - Display Helper classes
+
+                Adding 'smini-hide' class to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
+                Adding 'smini-show' class to an element will make it visible (opacity: 1) when the sidebar is in mini mode
+                    If you would like to disable the transition animation, make sure to also add the 'no-transition' class to your element
+
+                Adding 'smini-hidden' to an element will hide it when the sidebar is in mini mode
+                Adding 'smini-visible' to an element will show it (display: inline-block) only when the sidebar is in mini mode
+                Adding 'smini-visible-block' to an element will show it (display: block) only when the sidebar is in mini mode
+            -->
+            <nav id="sidebar" aria-label="Main Navigation">
+                <!-- Side Header -->
+                <div class="bg-header-dark">
+                    <div class="content-header bg-white-10">
+                        <!-- Logo -->
+                        <a class="font-w600 text-white tracking-wide" href="/">
+                            <span class="smini-visible">
+                                C<span class="opacity-75">s</span>
+                            </span>
+                            <span class="smini-hidden">
+                                Chemist<span class="opacity-75"> Fun Days</span>
+                            </span>
+                        </a>
+                        <!-- END Logo -->
+
+                        <!-- Options -->
+                        <div>
+                            <!-- Toggle Sidebar Style -->
+                            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                            <!-- Class Toggle, functionality initialized in Helpers.coreToggleClass() -->
+                            <a class="js-class-toggle text-white-75" data-target="#sidebar-style-toggler" data-class="fa-toggle-off fa-toggle-on" onclick="Dashmix.layout('sidebar_style_toggle');Dashmix.layout('header_style_toggle');" href="javascript:void(0)">
+                                <i class="fa fa-toggle-off" id="sidebar-style-toggler"></i>
+                            </a>
+                            <!-- END Toggle Sidebar Style -->
+
+                            <!-- Close Sidebar, Visible only on mobile screens -->
+                            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                            <a class="d-lg-none text-white ml-2" data-toggle="layout" data-action="sidebar_close" href="javascript:void(0)">
+                                <i class="fa fa-times-circle"></i>
+                            </a>
+                            <!-- END Close Sidebar -->
+                        </div>
+                        <!-- END Options -->
+                    </div>
+                </div>
+                <!-- END Side Header -->
+
+                <!-- Sidebar Scrolling -->
+                <div class="js-sidebar-scroll">
+                <!-- Side Navigation -->
+                    <div class="content-side content-side-full">
+                        <ul class="nav-main">
+                            <li class="nav-main-item">
+                                <a class="nav-main-link{{ request()->is('dashboard') ? ' active' : '' }}" href="/dashboard">
+                                    <i class="nav-main-link-icon fa fa-location-arrow"></i>
+                                    <span class="nav-main-link-name">Beranda</span>
+                                    <span class="nav-main-link-badge badge badge-pill badge-success">5</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-heading">Anda ingin keluar akun?</li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" href="{{ route('logout') }}">
+                                    <i class="nav-main-link-icon fa fa-arrow-alt-circle-left"></i>
+                                    <span class="nav-main-link-name">Keluar</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END Side Navigation -->
+                </div>
+                <!-- END Sidebar Scrolling -->
+            </nav>
+            <!-- END Sidebar -->
+
+            <!-- Header -->
+            <header id="page-header">
+                <!-- Header Content -->
+                <div class="content-header">
+                    <!-- Left Section -->
+                    <div>
+                        <!-- Toggle Sidebar -->
+                        <!-- Layout API, functionality initialized in Template._uiApiLayout()-->
+                        <button type="button" class="btn btn-dual" data-toggle="layout" data-action="sidebar_toggle">
+                            <i class="fa fa-fw fa-bars"></i>
+                        </button>
+                        <!-- END Toggle Sidebar -->
+
+                        <!-- Open Search Section -->
+                        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                        <button type="button" class="btn btn-dual" data-toggle="layout" data-action="header_search_on">
+                            <i class="fa fa-fw fa-search"></i> <span class="ml-1 d-none d-sm-inline-block">Pencarian</span>
+                        </button>
+                        <!-- END Open Search Section -->
+                    </div>
+                    <!-- END Left Section -->
+
+                    <!-- Right Section -->
+                    <div>
+                        <!-- User Dropdown -->
+                        <div class="dropdown d-inline-block">
+                            <button type="button" class="btn btn-dual" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa fa-fw fa-user d-sm-none"></i>
+                                <span class="d-none d-sm-inline-block">{{Auth::user()->name}}</span>
+                                <i class="fa fa-fw fa-angle-down ml-1 d-none d-sm-inline-block"></i>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right p-0" aria-labelledby="page-header-user-dropdown">
+                                <div class="bg-primary-darker rounded-top font-w600 text-white text-center p-3">
+                                   Pengaturan akun
+                                </div>
+                                <div class="p-2">
+                                    <a class="dropdown-item" href="javascript:void(0)">
+                                        <i class="far fa-fw fa-user mr-1"></i> Profil
+                                    </a>
+                                    <div role="separator" class="dropdown-divider"></div>
+
+                                    <div role="separator" class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        <i class="far fa-fw fa-arrow-alt-circle-left mr-1"></i> Keluar
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END User Dropdown -->
+
+                        <!-- Notifications Dropdown -->
+                        <div class="dropdown d-inline-block">
+                            <button type="button" class="btn btn-dual" id="page-header-notifications-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa fa-fw fa-bell"></i>
+                                {{-- untuk zaki nanti ketika notifikasi kosong angka pada pill berubah --}}
+                                <span class="badge badge-secondary badge-pill">3</span>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0" aria-labelledby="page-header-notifications-dropdown">
+                                <div class="bg-primary-darker rounded-top font-w600 text-white text-center p-3">
+                                   Notifications
+                                </div>
+                                <ul class="nav-items my-2">
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-check-circle text-success"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">Contoh notif berkas diverifikasi</div>
+                                                <div class="text-muted font-italic">3 menit yang lalu</div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-times-circle text-danger"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">Berkas ditolak pada saat ferivikasi</div>
+                                                <div class="text-muted font-italic">30 menit yang lalu</div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-exclamation-circle text-warning"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">Pemberitahuan ujian</div>
+                                                <div class="text-muted font-italic">1 jam yang lalu</div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <div class="p-2 border-top">
+                                    <a class="btn btn-light btn-block text-center" href="javascript:void(0)">
+                                        <i class="fa fa-fw fa-eye mr-1"></i> View All
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END Notifications Dropdown -->
+
+                        <!-- Toggle Side Overlay -->
+                    </div>
+                    <!-- END Right Section -->
+                </div>
+                <!-- END Header Content -->
+
+                <!-- Header Search -->
+                <div id="page-header-search" class="overlay-header bg-primary">
+                    <div class="content-header">
+                        <form class="w-100" action="/dashboard" method="POST">
+                            @csrf
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                                    <button type="button" class="btn btn-primary" data-toggle="layout" data-action="header_search_off">
+                                        <i class="fa fa-fw fa-times-circle"></i>
+                                    </button>
+                                </div>
+                                <input type="text" class="form-control border-0" placeholder="Search or hit ESC.." id="page-header-search-input" name="page-header-search-input">
+                            </div>
+                        </form>
+                   </div>
+                </div>
+                <!-- END Header Search -->
+
+                <!-- Header Loader -->
+                <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
+                <div id="page-header-loader" class="overlay-header bg-primary-darker">
+                    <div class="content-header">
+                        <div class="w-100 text-center">
+                            <i class="fa fa-fw fa-2x fa-sun fa-spin text-white"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Header Loader -->
+            </header>
+            <!-- END Header -->
+
+            <!-- Main Container -->
+            <main id="main-container">
+                @yield('content')
+            </main>
+            <!-- END Main Container -->
+
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Dashmix Core JS -->
+        <script src="{{ mix('js/dashmix.app.js') }}"></script>
+
+        <!-- Laravel Scaffolding JS -->
+        <!-- <script src="{{ mix('/js/laravel.app.js') }}"></script> -->
+
+        @yield('js_after')
+        <script type="text/javascript">
+        $(document).ready(function() {
+            $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+        });
+        </script>
+    </body>
+</html>
